38de9be31c30cd8f4f2687f8d486de8f1eca90dc
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-06-25 02:08:05 +0800

    feat: enable isolatedDeclarations

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,28 @@
-import { createUnplugin } from 'unplugin'
+import { type UnpluginInstance, createUnplugin } from 'unplugin'
 import { createFilter } from '@rollup/pluginutils'
 import { type Options, resolveOptions } from './core/options'
 
-export default createUnplugin<Options | undefined, false>((rawOptions = {}) => {
-  const options = resolveOptions(rawOptions)
-  const filter = createFilter(options.include, options.exclude)
+const plugin: UnpluginInstance<Options | undefined, false> = createUnplugin(
+  (rawOptions = {}) => {
+    const options = resolveOptions(rawOptions)
+    const filter = createFilter(options.include, options.exclude)
 
-  const name = 'unplugin-starter'
-  return {
-    name,
-    enforce: options.enforce,
+    const name = 'unplugin-starter'
+    return {
+      name,
+      enforce: options.enforce,
 
-    transformInclude(id) {
-      return filter(id)
-    },
+      transformInclude(id) {
+        return filter(id)
+      },
 
-    transform(code, id) {
-      // eslint-disable-next-line no-console
-      console.log(code, id)
-      return undefined
-    },
-  }
-})
+      transform(code, id) {
+        // eslint-disable-next-line no-console
+        console.log(code, id)
+        return undefined
+      },
+    }
+  },
+)
+
+export default plugin
