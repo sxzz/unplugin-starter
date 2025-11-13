ad5819cbc0a83b022d7e1fc2a2e008cad88d0677
Kevin Deng <sxzz@sxzz.moe>
2025-11-14 02:45:15 +0800

    perf: use native filter

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,23 @@
 import { createUnplugin, type UnpluginInstance } from 'unplugin'
-import { createFilter } from 'unplugin-utils'
 import { resolveOptions, type Options } from './core/options'
 
 export const Starter: UnpluginInstance<Options | undefined, false> =
   createUnplugin((rawOptions = {}) => {
     const options = resolveOptions(rawOptions)
-    const filter = createFilter(options.include, options.exclude)
 
     const name = 'unplugin-starter'
     return {
       name,
       enforce: options.enforce,
 
-      transformInclude(id) {
-        return filter(id)
-      },
-
-      // eslint-disable-next-line unused-imports/no-unused-vars
-      transform(code, id) {
-        return `// unplugin-starter injected\n${code}`
+      transform: {
+        filter: {
+          id: { include: options.include, exclude: options.exclude },
+        },
+        // eslint-disable-next-line unused-imports/no-unused-vars
+        handler(code, id) {
+          return `// unplugin-starter injected\n${code}`
+        },
       },
     }
   })
